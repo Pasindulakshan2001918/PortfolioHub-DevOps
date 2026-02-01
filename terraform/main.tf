e69b7c4b8e868696df39fd62a9a7ebc10e6b030d
Pasindu Lakshan <lpasindu923@gmail.com>
2026-02-01 14:42:01 +0530

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,10 @@ resource "aws_security_group" "portfoliohub_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    prevent_destroy = false  # allows Terraform to safely update/destroy if needed
+  }
 }
 resource "aws_instance" "portfoliohub_ec2" {
   ami           = "ami-0f5ee92e2d63afc18" # Ubuntu 22.04 (ap-south-1)
