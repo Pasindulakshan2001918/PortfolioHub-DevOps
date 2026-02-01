d2be0cfd193640c758ecf335ae021fd7276d5519
Pasindu Lakshan <lpasindu923@gmail.com>
2026-02-01 19:50:18 +0530

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,30 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# 🔹 Read EXISTING Security Group (already created in AWS)
+# 🔹 Read EXISTING Security Group
 data "aws_security_group" "portfoliohub_sg" {
   name   = "portfoliohub-sg"
   vpc_id = "vpc-07e5875498eb3ffa1"
+}
+
+# 🔹 Add HTTP rule to existing Security Group (port 80)
+resource "aws_security_group_rule" "allow_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = data.aws_security_group.portfoliohub_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# 🔹 Add SSH rule if you want to connect manually (optional, port 22)
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  security_group_id = data.aws_security_group.portfoliohub_sg.id
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 # 🔹 EC2 Instance
